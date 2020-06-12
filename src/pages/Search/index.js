06c53e71b3918e331d5251ee6c8c200020424431
juliastefanoni <juliastefanonidev@gmail.com>
2020-06-12 14:09:05 -0300

    Add action of user search

--- src/pages/Search/index.js
+++ src/pages/Search/index.js
@@ -1,10 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import swal from 'sweetalert';
+
+import api from '../../services/api';
 
 import './styles.css';
 
 import codeImg from '../../assets/code.svg';
 
 const Search = () => {
+  const [username, setUserName] = useState('');
+
+  const history = useHistory();
+
+  async function handleSearch(e) {
+    e.preventDefault();
+
+    if (!username) {
+      swal({
+        icon: 'error',
+        title: 'Ops!',
+        text: 'Enter username to continue.',
+        buttons: false,
+        timer: 2000,
+      });
+      return;
+    }
+
+    try {
+      const user = await api.get(`/${username}`);
+      console.log(user);
+
+      localStorage.setItem('username', user.data.login);
+
+      history.push('/user');
+    } catch (error) {
+      swal({
+        icon: 'error',
+        title: 'Ops, user not found!',
+        text: 'Try again.',
+        buttons: false,
+        timer: 2000,
+      });
+    }
+  }
   return (
     <div className="container">
       <div className="container-logo">
@@ -13,9 +52,15 @@ const Search = () => {
       <div className="container-search">
         <h1> gitbusca </h1>
 
-        <input type="text" placeholder="Type username" />
+        <input
+          type="text"
+          placeholder="Type username"
+          onChange={(event) => setUserName(event.target.value)}
+        />
 
-        <button type="submit">Search</button>
+        <button type="submit" onClick={handleSearch}>
+          Search
+        </button>
       </div>
     </div>
   );
